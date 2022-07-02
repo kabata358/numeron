d46ae0dcda09d88dee4e256eef5b4eb1e0c58f1d
kabata358 <kabata358@gmail.com>
2022-07-02 20:32:06 +0900

    BITEを実装

--- numeron/src/numeron/Main.java
+++ numeron/src/numeron/Main.java
@@ -15,8 +15,8 @@ public class Main {
 		for (int i = 0; i < 3; i++){
 			// 数字をランダムに生成
 			answer[i] = rnd.nextInt(10);
+			// 生成した数字の重複確認
 			for (int j = 0; j < i; j++) {
-				// 生成した数字が重複した場合
 				if (answer[i] == answer[j]){
 					i--;
 					break;
@@ -24,32 +24,45 @@ public class Main {
 			}
 		}
 		
-		System.out.println(Arrays.toString(answer));
+		/* 答えを表示する
+		System.out.println(Arrays.toString(answer) + System.getProperty("line.separator"));
+		*/
 		
+		System.out.println("+------------------+");
+		System.out.println("|     ゲーム開始     |");
+		System.out.println("+------------------+" + System.getProperty("line.separator"));
 		
-		// 入力
-		Scanner scanner = new Scanner(System.in);
-		int inputNum;
+		// 入力回数をカウントする
+		int count = 0;
+		
+		// 3つの数字を入力する
+		String inputNum;
 		int num[] = new int[3];
+		Scanner scanner = new Scanner(System.in);
 		
+		// 3つの数字の入力と判定処理
 		while (true) {
 			System.out.print("3桁の数字を入力してください。　-> ");
-			inputNum = scanner.nextInt();
+			inputNum = scanner.next();
 			
-			// 入力した数字の桁数を確認
-			if (String.valueOf(inputNum).length() != 3) {
-				System.out.println("数字は3桁で入力してください。");
+			//★★★入力されたのが数字かの判定が必要
+			
+			
+			// 入力した文字列の桁数を確認
+			if (inputNum.length() != 3) {
+				System.out.println("数字は3桁で入力してください。" + System.getProperty("line.separator"));
 				continue;
 			}
 			
-			// 入力した数字を配列に格納
+			// 入力した文字列を数字として配列に格納
 			for (int i = 2; i >= 0; i--) {
-				num[i] = inputNum % 10;
-				inputNum /= 10;
+				num[i] = Integer.parseInt(inputNum) % 10;
+				inputNum = String.valueOf(Integer.parseInt(inputNum) / 10);
 			}
 			
-			// 入力した数字が重複していないか確認
+			// 入力した3つの数字の重複確認
 			boolean flag = true;
+			
 			for (int i = 0; i < 3; i++){
 				for (int j = 0; j < i; j++) {
 					// 入力した数字が重複している場合
@@ -58,29 +71,42 @@ public class Main {
 					}
 				}
 			}
+			
 			if (flag == false) {
-				System.out.println("同じ数字は入力しないでください。");
+				System.out.println("同じ数字は入力しないでください。" + System.getProperty("line.separator"));
 				continue;
 			}
 			
-			// 判定
+			// EAT, BITE 判定
 			int eat = 0;
+			int bite = 0;
 			
 			for (int i = 0; i < 3; i++) {
-				if (num[i] == answer[i]) {
-					eat++;
+				for (int j = 0; j < 3; j++) {
+					// 入力した数字が答えと一致している場合
+					if (num[i] == answer[j]) {
+						if (i == j) {	// 入力した数字の桁が一致している場合
+							eat++;
+						} else {		// 入力した数字の桁が一致していない場合
+							bite++;
+						}
+					}
 				}
 			}
 			
-			System.out.println(eat + "EAT ");
+			System.out.println("  【 " + eat + " EAT  /  " + bite + " BITE" + " 】" + System.getProperty("line.separator"));
+			count++;
 			
+			// 3EAT でゲーム終了
 			if (eat == 3) {
 				break;
 			}
 		}
 		
-		System.out.println("ゲーム終了");
-		
+		System.out.println("+------------------+");
+		System.out.println("|     ゲーム終了     |");
+		System.out.println("+------------------+");
+		System.out.println(count + "回目で正解しました。");
 	}
 
 }
